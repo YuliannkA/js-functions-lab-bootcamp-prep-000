a0ac9d76a06efc9a0a99ab4a73de4db7df37e3e3
Yulia Drozdova <yuliannka@gmail.com>
2018-01-10 20:13:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, `${name}`!'
+  return `'Happy holidays, `${name}`!'`
+}
+
+function happyHolidayTo(holiday, name) {
+  
 }
